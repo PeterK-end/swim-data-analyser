85e07a935b34ef0ff9096836c9478aa13a459158
Peter Kannewitz <peter.kannewitz@posteo.net>
2024-09-03 19:17:26 +0200

    init upload logic for fit files

--- app/routes.py
+++ app/routes.py
@@ -24,14 +24,37 @@ data = [
     {"label": "C", "value": 20}
 ]
 
+def parse_fit_file(file_path):
+
+    data_blocks = {}
+
+    with fitdecode.FitReader(file_path) as fit:
+        for frame in fit:
+            if isinstance(frame, fitdecode.records.FitDataMessage):
+                block_name = frame.name
+
+                # Extract the fields
+                data = {field.name: field.value for field in
+                        frame.fields if 'unknown_' not in field.name}
+
+                # Append to the corresponding block list
+                if block_name not in data_blocks:
+                    data_blocks[block_name] = []
+
+                data_blocks[block_name].append(data)
+
+    # !!! not ready, testing
+    return data_blocks['sport'][0]
+
 def get_user_upload_folder():
     user_id = session.get('user_id', 'guest')
     user_folder = os.path.join(UPLOAD_FOLDER, user_id)
     os.makedirs(user_folder, exist_ok=True)
     return user_folder
+
 @main.route('/')
 def index():
-    return render_template('index.html', data=data)
+ return render_template('index.html', data=data)
 
 @main.route('/merge', methods=['POST'])
 def merge():
