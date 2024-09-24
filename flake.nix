a5025ffb7298164d6df7b9926bcc4344747e7d1d
Peter Kannewitz <peter.kannewitz@posteo.net>
2024-09-24 23:42:29 +0200

    adds nix flake framework for npm

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Project Dependencies for swim-data-analyser";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = inputs:
+  let
+    system = "x86_64-linux";
+    pkgs = inputs.nixpkgs.legacyPackages.${system};
+    python-env = pkgs.python310.withPackages (pp: with pp;
+    [
+      flask
+      flask-session
+    ]);
+   in {
+     devShell."${system}" = pkgs.mkShell {
+       buildInputs =  with pkgs; [
+         nodejs
+         nodePackages.webpack
+         nodePackages.webpack-cli
+       ] ++ [python-env];
+       shellHook = ''
+           echo 'loaded python'
+        '';
+     };
+   };
+}
