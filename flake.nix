59e685003d24bfd2efa4e6a2a5f2502a5209cfc0
Peter Kannewitz <peter.kannewitz@posteo.net>
2024-10-28 17:41:34 +0100

    flak.nix: adds deployement attribute (initial draft)

--- flake.nix
+++ flake.nix
@@ -5,25 +5,53 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
-  outputs = inputs:
-  let
-    system = "x86_64-linux";
-    pkgs = inputs.nixpkgs.legacyPackages.${system};
-    python-env = pkgs.python312.withPackages (pp: with pp;
-    [
-      django
-      gunicorn # WSGI HTTP Server for UNIX, fast clients and sleepy applications
-    ]);
-   in {
-     devShell."${system}" = pkgs.mkShell {
-       buildInputs =  with pkgs; [
-         nodejs
-         nodePackages.webpack
-         nodePackages.webpack-cli
-       ] ++ [python-env];
-       shellHook = ''
-           echo 'Dependencies loaded to environment.'
-        '';
-     };
-   };
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+      python-env = pkgs.python312.withPackages (pp: with pp; [
+        django
+        gunicorn # WSGI HTTP Server for UNIX, fast clients and sleepy applications
+      ]);
+    in {
+      # Define both development and deployment shells under devShells
+      devShells.${system} = {
+        # Development shell
+        default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs
+            nodePackages.webpack
+            nodePackages.webpack-cli
+          ] ++ [python-env];
+          shellHook = ''
+             echo 'Dependencies loaded to development environment.'
+          '';
+        };
+
+        # Deployment shell with runtime dependencies and JS asset build
+        deployment = pkgs.mkShell {
+          buildInputs = [
+            python-env
+            pkgs.nodejs
+            pkgs.nodePackages.webpack
+            pkgs.nodePackages.webpack-cli
+          ];
+
+          shellHook = ''
+            # Build static files with Webpack for production
+            echo "Building JS and CSS assets..."
+            npm install  # Install Node packages defined in package.json
+            npx webpack  # Run Webpack to generate production assets
+
+            # Django collectstatic for production
+            python manage.py collectstatic --noinput
+
+            # Run Gunicorn server
+            echo "Starting Gunicorn..."
+            ${python-env}/bin/gunicorn --bind 0.0.0.0:8000 swim_data_analyser.wsgi:application &
+            echo "Gunicorn is running on 0.0.0.0:8000."
+         '';
+        };
+      };
+    };
 }
