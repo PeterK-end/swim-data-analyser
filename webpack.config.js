95f0dd40180472b34188f04bc484618b782bed6e
Peter Kannewitz <peter.kannewitz@posteo.net>
2024-10-28 17:39:39 +0100

    webpack.config.js: fixes paths after django switch

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
 const path = require('path');
 
 module.exports = {
-    entry: './app/static/js/index.js', // Entry point for your JavaScript
+    entry: './swim_data_analyser/static/js/index.js', // Entry point for your JavaScript
     output: {
         filename: 'main.js', // Output bundled file name
-        path: path.resolve(__dirname, 'app/static/js/dist'), // Output directory
+        path: path.resolve(__dirname, 'swim_data_analyser/static/js/dist'), // Output directory
     },
     mode: 'development',
 };
