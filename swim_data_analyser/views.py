fd75f5ea2ad43fe90162dff9332ea28d3cc7ba5d
Peter Kannewitz <peter.kannewitz@posteo.net>
2025-05-17 18:07:04 +0200

    swimdata: python encode: improves fit file encoding
    - adds message indices
    - adds missing timestamp information
    - reorders according to fitfileviewer for easier debugging

--- swim_data_analyser/views.py
+++ swim_data_analyser/views.py
@@ -18,7 +18,7 @@ from fit_tool.profile.messages.length_message import LengthMessage
 from fit_tool.profile.messages.record_message import RecordMessage
 from fit_tool.profile.messages.session_message import SessionMessage
 from fit_tool.profile.messages.sport_message import SportMessage
-from fit_tool.profile.profile_type import FileType, Manufacturer, Event, EventType, Sport, SubSport, SessionTrigger, DisplayMeasure, LengthType, LapTrigger, SwimStroke
+from fit_tool.profile.profile_type import *
 from fit_tool.record import Record
 from fit_tool.utils.crc import crc16
 from pathlib import Path
@@ -106,12 +106,15 @@ def encode_js_object_to_fit(request):
             if 'activity' in js_object:
                 event_activity = js_object.get('activity', [])
                 message = ActivityMessage()
-                # message.local_timestamp = encode_time(event_activity.get('local_timestamp', current_time))
-                message.event = Event[event_activity.get('event', 'ACTIVITY').upper()]
-                message.event_type = EventType[event_activity.get('event_type', 'STOP').upper()]
-                message.num_sessions = event_activity.get('num_sessions', 0)
+
                 message.timestamp = encode_time(event_activity.get('timestamp', current_time))
                 message.total_timer_time = event_activity.get('total_timer_time', 0)
+                message.num_sessions = event_activity.get('num_sessions', 0)
+                message.type = Activity[event_activity.get('type', 0).upper()]
+                message.event = Event[event_activity.get('event', 'ACTIVITY').upper()]
+                message.event_type = EventType[event_activity.get('event_type', 'STOP').upper()]
+                # message.local_timestamp = encode_time(event_activity.get('local_timestamp', current_time))
+
                 definition = DefinitionMessage.from_data_message(message)
                 write_message(tmp_file, definition)
                 write_message(tmp_file, message)
@@ -162,31 +165,45 @@ def encode_js_object_to_fit(request):
             if 'sessions' in js_object:
                 event_session = js_object.get('sessions', [])[0]
                 message = SessionMessage()
-                message.avg_cadence = event_session.get('avg_cadence', 0)
-                message.avg_heart_rate = event_session.get('avg_heart_rate', 0)
-                message.avg_stroke_distance = event_session.get('avg_stroke_distance', 0)
-                message.enhanced_avg_speed = event_session.get('enhanced_avg_speed', 0)/ 3.6
-                message.enhanced_max_speed = event_session.get('enhanced_max_speed', 0)/ 3.6
+
+                message.timestamp = encode_time(event_session.get('timestamp', current_time))
+                message_index = event_session.get('message_index', 0)
+                message.message_index = message_index.get('value', 0)
                 message.event = Event[event_session.get('event', 'SESSION').upper()]
                 message.event_type = EventType[event_session.get('event_type', 'STOP').upper()]
-                message.first_lap_index = event_session.get('first_lap_index', 0)
-                message.max_heart_rate = event_session.get('max_heart_rate', 0)
-                message.num_active_lengths = event_session.get('num_active_lengths', 0)
-                message.num_laps = event_session.get('num_laps', 0)
-                message.pool_length = event_session.get('pool_length', 0)
-                message.pool_length_unit = DisplayMeasure[event_session.get('pool_length_unit', 'METRIC').upper()]
+                message.start_time = encode_time(event_session.get('start_time', current_time))
                 message.sport = Sport[event_session.get('sport', 'SWIMMING').upper()]
                 message.sub_sport = SubSport[event_session.get('sub_sport', 'LAP_SWIMMING').upper()]
-                message.start_time = encode_time(event_session.get('start_time', current_time))
-                message.total_anaerobic_effect = event_session.get('total_anaerobic_effect', 0)
-                message.total_calories = event_session.get('total_calories', 0)
-                message.total_cycles = event_session.get('total_cycles', 0)
-                message.total_distance = event_session.get('total_distance', 0)
                 message.total_elapsed_time = event_session.get('total_elapsed_time', 0)
-                message.total_strokes = event_session.get('total_strokes', 0)
                 message.total_timer_time = event_session.get('total_timer_time', 0)
+                message.total_distance = event_session.get('total_distance', 0)
+                message.total_cycles = event_session.get('total_cycles', 0)
+                message.total_calories = event_session.get('total_calories', 0)
+                message.avg_heart_rate = event_session.get('avg_heart_rate', 0)
+                message.max_heart_rate = event_session.get('max_heart_rate', 0)
+                message.avg_cadence = event_session.get('avg_cadence', 0)
                 message.total_training_effect = event_session.get('total_training_effect', 0)
+                message.first_lap_index = event_session.get('first_lap_index', 0)
+                message.num_laps = event_session.get('num_laps', 0)
                 message.trigger = SessionTrigger[event_session.get('trigger', 'ACTIVITY_END').upper()]
+                message.num_lengths = event_session.get('num_lengths', 0)
+                message.avg_stroke_distance = event_session.get('avg_stroke_distance', 0)
+                message.pool_length = event_session.get('pool_length', 0)
+                message.pool_length_unit = DisplayMeasure[event_session.get('pool_length_unit', 'METRIC').upper()]
+                message.num_active_lengths = event_session.get('num_active_lengths', 0)
+                # avg_swim_cadence?
+                # avg_swolf?
+                # sport_profile_name?
+                message.enhanced_avg_speed = event_session.get('enhanced_avg_speed', 0)/ 3.6
+                message.enhanced_max_speed = event_session.get('enhanced_max_speed', 0)/ 3.6
+                message.total_anaerobic_effect = event_session.get('total_anaerobic_effect', 0)
+                # training_loag_peak?
+                # primary_benefit?
+                # workout feel?
+                # workout_rpe?
+                # resting calories?
+                # recovery_hear_rate?
+                message.total_strokes = event_session.get('total_strokes', 0)
 
                 definition = DefinitionMessage.from_data_message(message)
                 write_message(tmp_file, definition)
@@ -199,29 +216,34 @@ def encode_js_object_to_fit(request):
                     try:
                         message = LengthMessage()
                         length_type = length_data.get('length_type', 'IDLE').upper()
+
                         if length_type == "IDLE":
+                            message.timestamp = encode_time(length_data.get('timestamp'))
+                            message_index = length_data.get('message_index', 0)
+                            message.message_index = message_index.get('value', 0)
                             message.event = Event[length_data.get('event', 'LENGTH').upper()]
                             message.event_type = EventType[length_data.get('event_type', 'STOP').upper()]
-                            message.length_type = LengthType[length_type]
                             message.start_time = encode_time(length_data.get('start_time'))
-                            message.timestamp = encode_time(length_data.get('timestamp'))
                             message.total_elapsed_time = length_data.get('total_elapsed_time', 0)
                             message.total_timer_time = length_data.get('total_timer_time', 0)
+                            message.length_type = LengthType[length_type]
                             definition = DefinitionMessage.from_data_message(message)
                             write_message(tmp_file, definition)
 
                         if length_type == "ACTIVE":
-                            message.avg_speed = length_data.get('avg_speed', 0) / 3.6 # m/s to km/h scaling factor
-                            message.avg_swimming_cadence = length_data.get('avg_swimming_cadence', 0)
+                            message.timestamp = encode_time(length_data.get('timestamp'))
+                            message_index = length_data.get('message_index', 0)
+                            message.message_index = message_index.get('value', 0)
                             message.event = Event[length_data.get('event', 'LENGTH').upper()]
                             message.event_type = EventType[length_data.get('event_type', 'STOP').upper()]
-                            message.length_type = LengthType[length_type]
-                            message.swim_stroke = SwimStroke[length_data.get('swim_stroke', 'MIXED').upper()]
                             message.start_time = encode_time(length_data.get('start_time'))
-                            message.timestamp = encode_time(length_data.get('timestamp'))
                             message.total_elapsed_time = length_data.get('total_elapsed_time', 0)
-                            message.total_strokes = length_data.get('total_strokes', 0)
                             message.total_timer_time = length_data.get('total_timer_time', 0)
+                            message.total_strokes = length_data.get('total_strokes', 0)
+                            message.avg_speed = length_data.get('avg_speed', 0) / 3.6 # m/s to km/h scaling factor
+                            message.swim_stroke = SwimStroke[length_data.get('swim_stroke', 'MIXED').upper()]
+                            message.avg_swimming_cadence = length_data.get('avg_swimming_cadence', 0)
+                            message.length_type = LengthType[length_type]
                             definition = DefinitionMessage.from_data_message(message)
                             write_message(tmp_file, definition)
 
@@ -238,28 +260,33 @@ def encode_js_object_to_fit(request):
                 for index, lap_data in enumerate(laps_data):
                     try:
                         message = LapMessage()
+                        message_index = lap_data.get('message_index', 0)
 
-                        message.avg_cadence = lap_data.get('avg_cadence', 0)
-                        message.avg_heart_rate = lap_data.get('avg_heart_rate', 0)
-                        message.avg_stroke_distance = lap_data.get('avg_stroke_distance', 0)
-                        message.enhanced_avg_speed = lap_data.get('enhanced_avg_speed', 0) / 3.6
-                        message.enhanced_max_speed = lap_data.get('enhanced_max_speed', 0) / 3.6
+                        message.timestamp = encode_time(lap_data.get('timestamp'))
+                        message.message_index = message_index.get('value', 0)
                         message.event = Event[lap_data.get('event', 'LAP').upper()]
                         message.event_type = EventType[lap_data.get('event_type', 'STOP').upper()]
-                        message.first_length_index = lap_data.get('first_length_index', 0)
-                        message.lap_trigger = LapTrigger[lap_data.get('lap_trigger', 'MANUAL').upper()]
-                        message.max_heart_rate = lap_data.get('max_heart_rate', 0)
-                        message.num_active_lengths = lap_data.get('num_active_lengths', 0)
-                        message.num_lengths = lap_data.get('num_lengths', 0)
-                        message.sport = Sport[lap_data.get('sport', 'SWIMMING').upper()]
-                        message.sub_sport = SubSport[lap_data.get('sub_sport', 'LAP_SWIMMING').upper()]
                         message.start_time = encode_time(lap_data.get('start_time'))
-                        message.timestamp = encode_time(lap_data.get('timestamp'))
-                        message.total_calories = lap_data.get('total_calories', 0)
-                        message.total_cycles = lap_data.get('total_cycles', 0)
-                        message.total_distance = lap_data.get('total_distance', 0)
                         message.total_elapsed_time = lap_data.get('total_elapsed_time', 0)
                         message.total_timer_time = lap_data.get('total_timer_time', 0)
+                        message.total_distance = lap_data.get('total_distance', 0)
+                        message.total_cycles = lap_data.get('total_cycles', 0)
+                        message.total_calories = lap_data.get('total_calories', 0)
+                        message.avg_heart_rate = lap_data.get('avg_heart_rate', 0)
+                        message.max_heart_rate = lap_data.get('max_heart_rate', 0)
+                        message.avg_cadence = lap_data.get('avg_cadence', 0)
+                        message.lap_trigger = LapTrigger[lap_data.get('lap_trigger', 'MANUAL').upper()]
+                        message.sport = Sport[lap_data.get('sport', 'SWIMMING').upper()]
+                        message.num_lengths = lap_data.get('num_lengths', 0)
+                        message.first_length_index = lap_data.get('first_length_index', 0)
+                        message.avg_stroke_distance = lap_data.get('avg_stroke_distance', 0)
+                        message.swim_stroke = SwimStroke[lap_data.get('swim_stroke', 'MIXED').upper()]
+                        message.sub_sport = SubSport[lap_data.get('sub_sport', 'LAP_SWIMMING').upper()]
+                        message.num_active_lengths = lap_data.get('num_active_lengths', 0)
+                        # total_strokes
+                        # avg_swolf
+                        message.enhanced_avg_speed = lap_data.get('enhanced_avg_speed', 0) / 3.6
+                        message.enhanced_max_speed = lap_data.get('enhanced_max_speed', 0) / 3.6
 
                         # Define the message structure only for the first lap
                         if index == 0:
